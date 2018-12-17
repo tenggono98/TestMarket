99ca8c0c70c2ecb16d5935ba35caf8f49efbf22b
tenggono98 <tenggono98@gmail.com>
2018-12-17 17:15:30 +0700

    update

--- index.php
+++ index.php
@@ -8,22 +8,83 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
 
+    <link rel="stylesheet" href="mainstyle.css">
+
     <title>Hello, world!</title>
   </head>
   <body>
     
 
+  <div class="Header">
+  
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+        
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+  
+  
+  </div>
+  
+  
+  
+  
+  <div class="content_index">
 
 
+  
+      
+  <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="img/davatar.png" class="rounded-circle" width="200" >
 
-
-
+            <h1 class="display-4">Alfonso Tenggono</h1>
+            <p class="lead">Mahasiswa Sistem Informasi Binus University</p>
+        </div>
+    </div>
+  </div>
+  
+  
+  
+  <div Class="footer">
+  
+  
+  
+  </div>
     
+
+
+
+  
+
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
