1297fe59fc75bed290eb923476406792b9799b5c
tenggono98 <tenggono98@gmail.com>
2018-12-17 17:53:08 +0700

    update

--- About.php
+++ About.php
@@ -17,7 +17,7 @@
 
   <div class="Header">
   
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
         
         <div class="container">
             <a class="navbar-brand" href="#">Market</a>
@@ -44,9 +44,37 @@
   
   
   
-<div class="content_index">
+  <div class="content_index">
 
  
+  <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="img/davatar.png" class="rounded-circle" width="200" >
+
+            <h1 class="display-4">Alfonso Tenggono</h1>
+            <p class="lead">Mahasiswa Sistem Informasi Binus University</p>
+        </div>
+    </div>
+  </div>
+  
+
+  <div class="container">
+      <div class="row">
+        <div class="col text-center">
+            <h1>About</h1>
+        </div>
+      </div>
+
+      <div class="row text-left">
+          <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus perspiciatis maxime molestias in temporibus enim nihil sint neque ab, magni dolorum tempora, ad omnis a harum at, voluptates ex iusto!</p>
+          </div>
+          <div class="col">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eveniet repellendus ab commodi, asperiores quidem. Consectetur repellendus atque beatae aliquid magni error corrupti recusandae aliquam odio. Ipsa hic beatae nobis.</p>
+          </div>
+
+      </div>
+
 </div>
   
   
