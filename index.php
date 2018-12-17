867bcb931817850a805ffb0135f0eceb24b78356
tenggono98 <tenggono98@gmail.com>
2018-12-17 17:23:04 +0700

    update

--- index.php
+++ index.php
@@ -52,9 +52,7 @@
   
   <div class="content_index">
 
-
-  
-      
+ 
   <div class="jumbotron jumbotron-fluid">
         <div class="container text-center">
             <img src="img/davatar.png" class="rounded-circle" width="200" >
@@ -65,9 +63,35 @@
     </div>
   </div>
   
+
+  <div class="container">
+      <div class="row">
+        <div class="col text-center">
+            <h1>About</h1>
+        </div>
+      </div>
+
+      <div class="row text-left">
+          <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus perspiciatis maxime molestias in temporibus enim nihil sint neque ab, magni dolorum tempora, ad omnis a harum at, voluptates ex iusto!</p>
+          </div>
+          <div class="col">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eveniet repellendus ab commodi, asperiores quidem. Consectetur repellendus atque beatae aliquid magni error corrupti recusandae aliquam odio. Ipsa hic beatae nobis.</p>
+          </div>
+
+      </div>
+
+</div>
   
   
   <div Class="footer">
+
+  <div class="jumbotron jumbotron-fluid">
+        <div class="container text-left">
+            <p class="lead">By .Alfonso Tenggono</p>
+        </div>
+    </div>
+  </div>
   
   
   
